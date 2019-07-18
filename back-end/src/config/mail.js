c20381541871866e3081d809ecac7aca61e65c53
Ramon Oliveira <ramonomello@gmail.com>
2019-07-18 02:15:33 -0300

    add envio de email no cancelamento

---
+++ back-end/src/config/mail.js
@@ -0,0 +1,19 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 25,
+  secure: false,
+  auth: {
+    user: 'd4a6a3fe02f510',
+    pass: 'a83bb2b33d76b5',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/**
+ * Amazon SES
+ * Mailgun
+ * Sparkpost
+ * Mandril (Only for Mailchimp)
+ */
